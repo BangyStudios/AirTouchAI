a991f99f7260bc42a8516a25fcb890eede11176c
BangyStudios <bangystudios@gmail.com>
2024-12-20 20:07:52 +1000

    Moved API to git submodule.

--- api/__init__.py
+++
@@ -1,1 +0,0 @@
-from api.airtouch import AirTouch, AirTouchStatus
--- api/helper.py
+++
@@ -1,7 +0,0 @@
-#region Helper Functions 
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-#endregion
